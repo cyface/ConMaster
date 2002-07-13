41899c1e86cc6ba95208ca0c5d86295679256cc7
cyface <cyface>
2002-07-13 15:30:44 +0000

    Latest.

--- cm/lib/FormObject.php
+++ cm/lib/FormObject.php
@@ -59,129 +59,267 @@ class FormObject {
 				$this->included_table = $this->data['included_table'];
 				require_once('./lib/DataObjects/' . ucwords($this->included_table) . '.php');
 				$incObjectName = 'DataObjects_' . ucwords($this->included_table); //Name of DataObject subclass to use for data access
-			    $this->incDataObject = DB_DataObject::staticGet($incObjectName,$this->table . '_id',$this->data['id']); 
+			    $this->incDataObject = new $incObjectName; //Start with an empty included object.  It gets loaded in edit()
+				if ($this->data['included_order_by']) {
+					$this->incDataObject->orderBy($this->data['included_order_by']);
+				}
 			}
+			
+			$this->template = new TemplatePower('./templates/' . $this->table . '_' . 'edit' . '.html'); //make a new TemplatePower object with default 'Edit' template
+			$this->template->prepare();//let TemplatePower do its thing, parsing etc.;
+
 			return true;
 		}
 
-	}//End constructor FormObject
+	} //End constructor FormObject
 
 	/**
 	* processAction grabs the correct form template, connects to the DB if needed, and outputs the filled out form
 	*
 	*/
     function processAction () {
-		$this->template = new TemplatePower('./templates/' . $this->table . '_' . 'edit' . '.html'); //make a new TemplatePower object
-		$this->template->prepare();//let TemplatePower do its thing, parsing etc.;
-
-		switch($this->action){
-			case 'new':
-				$this->template->assign('action_message', '<font color="#66CC00">Create A New ' . ucwords(str_replace('_',' ',$this->table)) . '</font>');
-				break;
-			case 'edit':
-				$this->dataObject->get($this->data['id']);
-				if ($this->data['getLinks']) {
-				   $this->dataObject->getLinks(); 
-				}
-				objectValueFill($this->dataObject,$this->template);
-				if ($this->incDataObject) {
-					while($this->incDataObject->fetch()){ //Pull the results through the object and put them on the form
-						$this->incDataObject->getLinks();
-						//echo '<PRE> Object:'; print_r($this->incDataObject); echo '</PRE>';
-						$this->template->newBlock($this->included_table . '_row'); //create a new result_row block
-						objectValueFill($this->incDataObject,$this->template);
-					}
-				}
-				break;
-			case 'save':
-				$this->dataObject->setFrom($this->data); //Copy the form data into the object.
-				if ($this->dataObject->id) {
-				    $this->dataObject->update();
-				} else {
-					$this->dataObject->id = $this->dataObject->insert();
-				}
-				$this->template->assign('action_message', '<font color="#66CC00">Saved</font>');
-				if ($this->data['getLinks']) {
-				   $this->dataObject->getLinks(); 
-				}
-				objectValueFill($this->dataObject,$this->template);
-				if ($this->incDataObject) {
-					while($this->incDataObject->fetch()){ //Pull the results through the object and put them on the form
-						$this->incDataObject->getLinks();
-						//echo '<PRE> Object:'; print_r($this->incDataObject); echo '</PRE>';
-						$this->template->newBlock($this->included_table . '_row'); //create a new result_row block
-						objectValueFill($this->incDataObject,$this->template);
-					}
-				}
-	
-				//$this->template->assign('action_message', '<font color="#FF0000">Save Failed, Please Correct Errors</font>');
-				//objectValueFill($this->dataObject,$this->template); //Put the values back into the form
-				//errorFill($this->dataObject,$this->template); //Fill in the error message placeholders with the save errors
-				break;
-			case 'save_included': //TEST
-				$this->incDataObject->setFrom($this->data); //Copy the form data into the object.
-				if ($this->incDataObject->id) {
-				    $this->incDataObject->update();
-				} else {
-					$this->incDataObject->id = $this->incDataObject->insert();
-				}
-				$this->template->assign('action_message', '<font color="#66CC00">Saved</font>');
-				$this->dataObject->get($this->data['parent_id']);
-				objectValueFill($this->dataObject,$this->template);
-				break;
-			case 'delete':
-				if ($this->data['id'] == '') { //No id for this record
-					$this->dataObject->validation_results['form'] .= '<br> Tried to delete a non-existent record.';
-					$this->template->assign('action_message', '<font color="#FF0000">Delete Failed!</font>');
-					objectValueFill($this->dataObject,$this->template); //Put the values back into the form
-					errorFill($this->dataObject,$this->template); //Fill in the error message placeholders with the save errors
-					return false;
-				}
-					$this->dataObject->setFrom($this->data);
-					$this->dataObject->delete(); //Try to delete the record in the DB using the object
-				    $this->template = new TemplatePower('./templates/' . 'deleted.html'); //make a new TemplatePower objec
-					$this->template->prepare();//let TemplatePower do its thing, parsing etc.
-					$this->template->assign('action_message', '<font color="#66CC00">Deleted</font>');
-					objectValueFill($this->dataObject,$this->template);
-				break;
-			case 'search':
-				$this->template = new TemplatePower('./templates/' . $this->table . '_' . 'search' . '.html'); //make a new TemplatePower object
-				$this->template->prepare();//let TemplatePower do its thing, parsing etc.;
-
-				if ($this->data['search']) { //Only bother to search if they submit critera, show the blank form regardless
-				    valueFill($this->data['search'],$this->template); //Put the search values back in the form fields
-					foreach ($this->data['search'] as $field => $value) {
-						$whereLine = $this->buildWhereLine($field, $this->data['search_operators'][$field], $value);
-						if ($whereLine) {
-							$this->dataObject->whereAdd($whereLine);
-						}
-					}
-					$this->dataObject->find();
-						
-					while($this->dataObject->fetch()){ //Pull the results through the object and put them on the form
-						$this->dataObject->getLinks();
-						//echo '<PRE> Object:'; print_r($this->dataObject); echo '</PRE>';
-						$this->template->newBlock('result_row'); //create a new result_row block
-						objectValueFill($this->dataObject,$this->template);
-					}
-				}
-				break;
-			default:
-				return false;
-		} // switch
+		//Try and find a method of this object that matches this->action and run it
+		if (method_exists($this, $this->action)) {
+			return call_user_func(array(&$this, $this->action));
+		} else {
+			return false;
+		}
+	} //End function processAction
+		
+	/**
+	* edit shows the edit form for this object (a blank form if a new object)
+	*
+	*/
+	function edit() {
+		if ($this->data['getLinks']) {
+		   $this->dataObject->getLinks(); 
+		}
+		objectValueFill($this->dataObject,$this->template);
+		if ($this->incDataObject) {
+			$this->incDataObject->staticGet($this->table . '_id',$this->dataObject->id); //Try and load it with the current data
+			while($this->incDataObject->fetch()){ //Pull the results through the object and put them on the form
+				$this->incDataObject->getLinks();
+				$this->template->newBlock($this->included_table . '_row'); //create a new result_row block
+				objectValueFill($this->incDataObject,$this->template);
+			}
+		}
 		return true;
-	}//End function processAction
+	} //End function edit
+	
+	/**
+	* update copies the $this->data variables with matching names to this object, and updates them in the DB
+	*
+	*/
+	function save() {
+		$this->dataObject->setFrom($this->data); //Copy the form data into the object.
+		if ($this->dataObject->id) {
+		    $this->dataObject->update();
+		} else {
+			$this->dataObject->id = $this->dataObject->insert();
+		}
+		$this->template->assign('action_message', '<font color="#66CC00">Saved</font>');
+		$this->edit();
+		return true;
+	} //End function update
+	
+	/**
+	* saveIncluded copies the $this->data variables with matching names to this object's includedObject, and updates the included rec the DB
+	*
+	*/
+	function saveIncluded() {
+		$this->incDataObject->setFrom($this->data); //Copy the form data into the object.
+		if ($this->data['included_id']) {
+			$this->incDataObject->id = $this->data['included_id'];
+		    $this->incDataObject->update(); //Only update the DB if an id was supplied
+			$this->template->assign('action_message', '<font color="#66CC00">Updated</font>');
+		} else {
+			$this->incDataObject->id = $this->incDataObject->insert();
+			$this->template->assign('action_message', '<font color="#66CC00">Added</font>');
+		}
+		$this->dataObject->get($this->data['parent_id']);
+		$incClassName = get_class($this->incDataObject);
+		$this->edit();
+		return true;
+	} //End function updateIncluded
+	
+	/**
+	* delete copies the $this->data variables with matching names to this objec's includedObject, and deletes the matching included row in the DB
+	*
+	*/
+	function delete() {
+		if ($this->data['id'] == '') { //No id for this record
+			//$this->dataObject->validation_results['form'] .= '<br> Tried to delete a non-existent record.';
+			$this->template->assign('action_message', '<font color="#FF0000">Delete Failed!</font>');
+			$this->edit();
+			return false;
+		}
+		$this->dataObject->id = $this->data['id'];
+		$this->dataObject->delete(); //Try to delete the record in the DB using the object
+		$this->template = new TemplatePower('./templates/' . 'deleted.html'); //make a new TemplatePower objec
+		$this->template->prepare();//let TemplatePower do its thing, parsing etc.
+		return true;
+	} //End function delete
+	
+	/**
+	* delete copies the $this->data variables with matching names to this object, and deletes the matching row in the DB
+	*
+	*/
+	function deleteIncluded() {
+		if ($this->data['included_id'] == '') { //No id for this record
+			$this->dataObject->validation_results['form'] .= '<br> Tried to delete a non-existent record.';
+			$this->template->assign('action_message', '<font color="#FF0000">Delete Failed!</font>');
+			$this->dataObject->get($this->data['parent_id']);
+			$incClassName = get_class($this->incDataObject);
+			$this->incDataObject = new $incClassName; //reinit the object to get around DataObject bug
+			$this->edit();
+			return false;
+		}
+		$this->incDataObject->id = $this->data['included_id'];
+		$this->incDataObject->delete(); //Try to delete the record in the DB using the object
+		$this->template->assign('action_message', '<font color="#FF0000">Row Deleted</font>');
+		$this->dataObject->get($this->data['parent_id']);
+		$incClassName = get_class($this->incDataObject);
+		$this->incDataObject = new $incClassName; //reinit the object to get around DataObject bug
+		if ($this->data['included_order_by']) {
+			$this->incDataObject->orderBy($this->data['included_order_by']);
+		}
+		$this->edit();
+		return true;
+	} //End function delete
+	
+	/**
+	* search uses the $this->data(search[]) variables with matching names to this object, and returns matching rows from the DB
+	*
+	*/
+	function search() {
+		$this->template = new TemplatePower('./templates/' . $this->table . '_' . 'search' . '.html'); //make a new TemplatePower object
+		$this->template->prepare();//let TemplatePower do its thing, parsing etc.;
+		
+		if ($this->data['search']) { //Only bother to search if they submit critera, show the blank form regardless
+		    valueFill($this->data['search'],$this->template); //Put the search values back in the form fields
+			foreach ($this->data['search'] as $field => $value) {
+				$whereLine = $this->buildWhereLine($field, $this->data['search_operators'][$field], $value);
+				if ($whereLine) {
+					$this->dataObject->whereAdd($whereLine);
+				}
+			}
+			$this->dataObject->find();
+				
+			while($this->dataObject->fetch()){ //Pull the results through the object and put them on the form
+				$this->dataObject->getLinks();
+				//echo '<PRE> Object{'; print_r($this->dataObject); echo '</PRE>';
+				$this->template->newBlock('result_row'); //create a new result_row block
+				objectValueFill($this->dataObject,$this->template);
+			}
+		}
+		return true;
+	} //End function search
+	
+	
+	/**
+	* report pulls the $this->data columns, and displays the results with breaks and subtotals
+	*
+	*/
+	function report() {
+		//Using the arrays passed in via $this->data, set up the query
+		if ($this->data['selcols']) {
+			$this->dataObject->selectAdd(); //Clear the default '*'
+			foreach ($this->data['selcols'] as $colName) {
+				$this->dataObject->selectAdd($colName);
+			}
+		} else {
+			return false;
+		}
+		if ($this->data['groupcols']) {
+			foreach ($this->data['groupcols'] as $colName) {
+				$this->dataObject->groupBy($colName);
+			}
+		}
+		if ($this->data['ordercols']) {
+			foreach ($this->data['ordercols'] as $colName) {
+				$this->dataObject->orderBy($colName);
+			}
+		}
+		if ($this->data['where']) {
+			foreach ($this->data['where'] as $whereRow) {
+				$this->dataObject->whereAdd($whereRow);
+			}
+		}
+		if ($this->data['limit']) {
+			foreach ($this->data['limit'] as $limit) {
+				$this->dataObject->limit($limit);
+			}
+		}
+		
+		$this->dataObject->find(); //Run the query with the stuff we just set
 
+		$this->template = new TemplatePower( './templates/report_two_col.html' );//make a new TemplatePower object
+		$this->template->prepare();//let TemplatePower do its thing, parsing etc.
+		
+		foreach ($this->data['selcols'] as $colNum => $colName) {
+			$this->template->assign('col' . $colNum . 'Header',ucwords(str_replace('_',' ',$colName)));
+		}
+		
+		$lastCol1 = -1;
+		$subtotalCol1Col1 = 0;
+		$totalCol1Col1 = 0;
+		
+		while ($this->dataObject->fetch()) {
+			foreach ($this->data['selcols'] as $colNum => $colName) {
+				$varName = 'col' . $colNum;
+				$$varName = $this->dataObject->$colName; //Tricky.  See the 'variable variables section of the PHP manual
+			}
+			
+			//echo '<pre>Col1:'; echo print_r($col1); echo '</pre>';
+			
+			switch ($lastCol1) {
+				case -1:
+					$lastCol1 = $this->dataObject;
+					$this->template->newBlock('result_row');
+					$this->template->assign('col1',$col1);
+				break;
+				case ($col1):
+					$foo = 'state';
+					$this->template->newBlock('result_row');
+					$this->template->assign('col1','');
+				break;
+				default: //when col1 != $lastCol1
+					$this->template->newBlock("total_row");
+					$this->template->assign('col1',$lastCol1 . ' Subtotal');
+					$this->template->assign('col3',$subtotalCol1);
+					$subtotalCol1 = 0;
+					
+					$this->template->newBlock('result_row');
+					$this->template->assign('col1',$col1);
+				break;
+			} //end switch
+			$lastCol1 = $col1;
+			$subtotalCol1 += $col3;
+			$totalCol1 += $col3;
+			$this->template->assign('col2',$col2);
+			$this->template->assign('col3',$col3);
+		}
+		$this->template->newBlock("total_row"); //Final subtotal
+		$this->template->assign('col1',$lastCol1 . ' Subtotal');
+		$this->template->assign('col3',$subtotalCol1);
+		$this->template->newBlock("total_row"); //Final total
+		$this->template->assign('col1','Grand Total');
+		$this->template->assign('col3',$totalCol1);
+
+		return true;
+	} //End function report
+	
 	/**
 	* display calls the displayToScreen method of the template object
 	*
 	*/
     function display () {
 		$this->template->printToScreen();
-	}//End function display
+		return true;
+	} //End function display
 
 	/**
-	* display calls the displayToScreen method of the template object
+	* buildWhereLine takes a field, operator and value and builds a SQL where clause line
 	*
 	* @param string $inField - name of the field to compare to a value
 	* @param string $inOperator - the comparison name (as would appear on a search form)
@@ -226,7 +364,7 @@ class FormObject {
 		  		//Took care of this with our var init above
 		  		break;
 		} //End switch
-		return ($inField . $operator . $prefix . $inValue . $postfix);			
+		return ($inField . $operator . $prefix . $inValue . $postfix);	
 	} //End function buildWhereLine
 	
 } //End class FormObject
