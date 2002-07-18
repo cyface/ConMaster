ecbb6095e7a0315f3660f95c3eaeed6fb1e055e4
cyface <cyface>
2002-07-18 21:33:09 +0000

    Tweaked valueFill not to run if the array was empty.

--- cm/lib/TemplateHelpers.inc.php
+++ cm/lib/TemplateHelpers.inc.php
@@ -8,7 +8,7 @@
  *
  * These are mainly used by FormObject.php and form_merge.php
  *
- * CVS Info: $Id: TemplateHelpers.inc.php,v 1.7 2002/07/17 23:58:36 cyface Exp $
+ * CVS Info: $Id: TemplateHelpers.inc.php,v 1.8 2002/07/18 21:33:09 cyface Exp $
  *
  * This class is copyright (c) 2002 by Tim White
  * @author Tim White <tim@cyface.com>
@@ -32,13 +32,20 @@ function errorFill ($inObject, &$inTemplate)
     // Fill in the error message placeholders with the save errors
     foreach ($inObject->validation_results as $key => $value) {
         $inTemplate->assign($key . '_error' , stripslashes($value));
+		if (is_array($value)) {
+			foreach ($value as $subKey => $subValue) {
+				$inTemplate->assign($key . '[' . $subKey . ']', stripslashes($subValue));
+			}
+		} else {
+			$inTemplate->assign($key, stripslashes($value));
+		}
     }
 
     return true;
 }
 
 /**
- * objectValueFill
+ * objectValueFill assigns each property of an object to the matching spot on the form
  * @param Array $inDBObject - a DBObject object
  * @param TemplatePower $inTemplate - a TemplatePower Object that has already been inited
  * @see TemplatePower
@@ -56,45 +63,30 @@ function objectValueFill ($inDBObject, &$inTemplate)
             $inTemplate->assign($key, stripslashes($value));
         }
     }
-
-    /**
-     * foreach (get_object_vars($inDBObject) as $key => $value) {
-     * if ((strstr($key,'child_')) and (is_array($value))) {
-     * $nameParts = explode('_',$key);
-     * unset($nameParts[0]);
-     * foreach ($value as $row) {
-     * $inTemplate->newBlock(implode('_',$nameParts) . '_row'); //create a new result_row block
-     * foreach ($row as $childKey => $childValue) {
-     * valueFill($row,$inTemplate);
-     * }
-     * }
-     * }
-     * }
-     **/
-
     return true;
 }
 
 /**
- * valueFill
+ * valueFill assigns each element of an associative array to the matching spot on the form
  * @param Array $inHash - a hashtable of key value pairs to assign to spots on the form
  * @param TemplatePower $inTemplate - a TemplatePower Object that has already been inited
  * @see TemplatePower
  **/
-function valueFill ($inHash, &$inTemplate)
+function valueFill ($inHash, &$inTemplate)  //& means that we can change inTemplate
 {
-    // echo '<PRE>'; print_r ($inHash); echo '<PRE>';
-    // Put the values back into the form
-    foreach ($inHash as $key => $value) {
-        if (is_array($value)) {
-            foreach ($value as $subKey => $subValue) {
-                $inTemplate->assign($key . '[' . $subKey . ']', stripslashes($subValue));
-            }
-        } else {
-            $inTemplate->assign($key, stripslashes($value));
-        }
-    }
-
+	if ($inHash) { //Don't do anything if inHash is null
+	    //echo '<PRE>'; print_r ($inHash); echo '<PRE>'; //Testing Only
+	    // Put the values back into the form
+	    foreach ($inHash as $key => $value) {
+	        if (is_array($value)) {
+	            foreach ($value as $subKey => $subValue) {
+	                $inTemplate->assign($key . '[' . $subKey . ']', stripslashes($subValue));
+	            }
+	        } else {
+	            $inTemplate->assign($key, stripslashes($value));
+	        }
+	    }
+	}
     return true;
 }
 
