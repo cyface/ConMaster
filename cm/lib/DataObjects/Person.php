870d030ce7b4b647b3a54d830dd9a5fb9475f4b1
cyface <cyface>
2002-07-31 14:25:28 +0000

    Added the new fee tracking fields.

--- cm/lib/DataObjects/Person.php
+++ cm/lib/DataObjects/Person.php
@@ -12,23 +12,26 @@ class DataObjects_Person extends DB_DataObject {
 
     var $__table='person';                          // table name
     var $id;                              // int(9)  not_null primary_key unsigned auto_increment
-    var $first_name;                      // string(80)  
+    var $first_name;                      // string(80)
     var $last_name;                       // string(80)  multiple_key
-    var $middle_name;                     // string(80)  
-    var $badge_number;                    // int(6)  
-    var $street;                          // string(80)  
-    var $city;                            // string(80)  
-    var $state;                           // string(80)  
-    var $zip;                             // string(15)  
-    var $phone;                           // string(15)  
-    var $reg_type;                        // string(80)  
-    var $judge;                           // string(10)  
-    var $staff;                           // string(10)  
-    var $volunteer;                       // string(10)  
-    var $rpga_number;                     // int(9)  
-    var $total_fee;                       // real(10)  
-    var $email_address;                   // string(80)  
-    var $country;                         // string(30)  
+    var $middle_name;                     // string(80)
+    var $badge_number;                    // int(6)
+    var $street;                          // string(80)
+    var $city;                            // string(80)
+    var $state;                           // string(80)
+    var $zip;                             // string(15)
+    var $phone;                           // string(15)
+    var $reg_type;                        // string(80)
+    var $judge;                           // string(10)
+    var $staff;                           // string(10)
+    var $volunteer;                       // string(10)
+    var $rpga_number;                     // int(9)
+    var $total_fee;                       // real(10)
+    var $additional_fees;                 // real(10)
+    var $amount_paid;                     // real(10)
+    var $payment_type;                    // string(15)
+    var $email_address;                   // string(80)
+    var $country;                         // string(30)
     var $convention_id;                   // int(10)  not_null
     var $last_modified;                   // timestamp(14)  not_null unsigned zerofill timestamp
 
@@ -38,15 +41,15 @@ class DataObjects_Person extends DB_DataObject {
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
-	
+
 	function insert() {
 		//Grab the database connection that has already been made by the parent's constructor:
 		require_once('DB.php');
 		$connections = &PEAR::getStaticProperty('DB_DataObject','connections');
 		$__DB = &$connections[$this->_database_dsn_md5];
-		
+
 		$this->badge_number = $__DB->nextId('badge_number'); //Fetch the next id in the sequence.  To set the sequence to a specifc value, use phpMyAdmin to tweak the value in the badge_number_seq table
-		
+
 		return DB_DataObject::insert();
 	}
 }
