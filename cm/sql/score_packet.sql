030ea63d4276307a2425ad1274667c71b42a528e
cyface <cyface>
2002-07-31 14:28:23 +0000

    Lots of tweaks, notably increasing the RPGA event type field length.

--- cm/sql/score_packet.sql
+++ cm/sql/score_packet.sql
@@ -4,36 +4,37 @@
 # http://www.phpmyadmin.net/ (download page)
 #
 # Host: localhost
-# Generation Time: Jul 26, 2002 at 04:48 PM
-# Server version: 3.22.32
+# Generation Time: Jul 29, 2002 at 10:46 PM
+# Server version: 3.23.51
 # PHP Version: 4.1.2
-# Database : cyface
+# Database : `cyface`
 # --------------------------------------------------------
 
 #
-# Table structure for table score_packet
+# Table structure for table `score_packet`
 #
 
 DROP TABLE IF EXISTS score_packet;
 CREATE TABLE score_packet (
-   id int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-   person_id int(11) unsigned DEFAULT '0' NOT NULL,
-   event_id int(11) unsigned DEFAULT '0' NOT NULL,
-   section_id int(11) unsigned DEFAULT '0' NOT NULL,
-   scenario_score tinyint(4) unsigned,
-   group_score tinyint(4) unsigned,
-   number_of_players tinyint(4) unsigned,
-   rpga_event_type char(2),
-   convention_id int(11) unsigned DEFAULT '1' NOT NULL,
-   last_modified timestamp(14),
-   PRIMARY KEY (id)
-);
+  id int(11) unsigned NOT NULL auto_increment,
+  person_id int(11) unsigned NOT NULL default '0',
+  event_id int(11) unsigned NOT NULL default '0',
+  section_id int(11) unsigned NOT NULL default '0',
+  scenario_score tinyint(4) unsigned default NULL,
+  group_score tinyint(4) unsigned default NULL,
+  number_of_players tinyint(4) unsigned default NULL,
+  rpga_event_type char(2) default NULL,
+  convention_id int(11) unsigned NOT NULL default '1',
+  last_modified timestamp(14) NOT NULL,
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
 
 #
-# Dumping data for table score_packet
+# Dumping data for table `score_packet`
 #
 
-INSERT INTO score_packet VALUES (2, 20063, 107, 133, 73, 19, 5, 'BE', 0, 20020714160455);
+INSERT INTO score_packet VALUES (1, 1163, 107, 33, 61, 28, 6, 'BE', 1, 20020729224506);
+INSERT INTO score_packet VALUES (2, 20063, 107, 133, 73, 19, 5, 'BE', 1, 20020729224608);
 INSERT INTO score_packet VALUES (3, 995, 108, 138, 81, 23, 6, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (4, 501, 108, 138, 70, 27, 5, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (5, 71, 113, 155, 80, 24, 5, 'BE', 1, 20020714160455);
@@ -54,12 +55,12 @@ INSERT INTO score_packet VALUES (24, 84, 193, 269, 95, 26, 6, 'BE', 1, 200207141
 INSERT INTO score_packet VALUES (25, 1163, 109, 139, 93, 28, 6, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (27, 20084, 107, 132, 71, 25, 5, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (29, 20063, 107, 129, 69, 21, 5, 'BE', 1, 20020714160455);
-INSERT INTO score_packet VALUES (33, 568, 107, 129, 75, 26, 5, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (32, 397, 114, 158, 67, 30, 6, 'GM', 1, 20020714160455);
-INSERT INTO score_packet VALUES (35, 265, 109, 139, 91, 26, 6, 'BE', 1, 20020714160455);
-INSERT INTO score_packet VALUES (37, 71, 110, 145, 85, 27, 6, 'MA', 1, 20020714160455);
-INSERT INTO score_packet VALUES (36, 1163, 110, 145, 102, 30, 6, 'GM', 1, 20020714160455);
+INSERT INTO score_packet VALUES (33, 568, 107, 129, 75, 26, 5, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (34, 84, 122, 178, 97, 18, 6, 'BE', 1, 20020714160455);
+INSERT INTO score_packet VALUES (35, 265, 109, 139, 91, 26, 6, 'BE', 1, 20020714160455);
+INSERT INTO score_packet VALUES (36, 1163, 110, 145, 102, 30, 6, 'GM', 1, 20020714160455);
+INSERT INTO score_packet VALUES (37, 71, 110, 145, 85, 27, 6, 'MA', 1, 20020714160455);
 INSERT INTO score_packet VALUES (42, 351, 118, 167, 55, 26, 4, 'BE', 1, 20020726164842);
 INSERT INTO score_packet VALUES (43, 1061, 119, 171, 69, 28, 5, 'BE', 1, 20020726164842);
 INSERT INTO score_packet VALUES (44, 521, 113, 154, 75, 25, 5, 'BE', 1, 20020714160455);
@@ -68,12 +69,12 @@ INSERT INTO score_packet VALUES (48, 397, 113, 154, 58, 30, 4, 'BE', 1, 20020714
 INSERT INTO score_packet VALUES (50, 181, 116, 161, 65, 23, 4, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (53, 2049, 108, 134, 83, 20, 6, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (54, 192, 113, 154, 127, 81, 6, 'BE', 1, 20020714160455);
-INSERT INTO score_packet VALUES (64, 265, 109, 140, 97, 25, 6, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (56, 2065, 112, 150, 20, 20, 5, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (59, 1163, 111, 147, 105, 29, 6, 'GM', 1, 20020714160455);
 INSERT INTO score_packet VALUES (60, 20084, 109, 139, 102, 26, 6, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (61, 71, 118, 167, 55, 24, 4, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (62, 20538, 107, 131, 66, 30, 6, 'BE', 1, 20020714160455);
+INSERT INTO score_packet VALUES (64, 265, 109, 140, 97, 25, 6, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (65, 283, 121, 175, 86, 26, 6, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (66, 84, 122, 179, 82, 25, 5, 'BE', 1, 20020726164842);
 INSERT INTO score_packet VALUES (67, 20063, 107, 131, 57, 25, 5, 'BE', 1, 20020714160455);
@@ -87,8 +88,8 @@ INSERT INTO score_packet VALUES (76, 2065, 112, 149, 77, 30, 5, 'BE', 1, 2002071
 INSERT INTO score_packet VALUES (77, 20084, 117, 166, 87, 15, 6, 'BE', 1, 20020726164423);
 INSERT INTO score_packet VALUES (78, 1061, 127, 185, 93, 29, 6, 'BE', 1, 20020726164842);
 INSERT INTO score_packet VALUES (79, 351, 112, 149, 59, 22, 4, 'BE', 1, 20020714160455);
-INSERT INTO score_packet VALUES (81, 20538, 107, 130, 69, 28, 5, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (80, 85, 120, 173, 119, 15, 6, 'BE', 1, 20020714160455);
+INSERT INTO score_packet VALUES (81, 20538, 107, 130, 69, 28, 5, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (82, 568, 113, 153, 78, 28, 6, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (83, 534, 113, 152, 84, 30, 6, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (85, 521, 113, 152, 80, 16, 5, 'BE', 1, 20020714160455);
@@ -112,5 +113,5 @@ INSERT INTO score_packet VALUES (111, 2017, 121, 177, 82, 26, 6, 'BE', 1, 200207
 INSERT INTO score_packet VALUES (112, 2049, 113, 152, 68, 30, 5, 'BE', 1, 20020714160455);
 INSERT INTO score_packet VALUES (113, 1163, 111, 146, 88, 29, 6, 'GM', 1, 20020714160455);
 INSERT INTO score_packet VALUES (114, 397, 113, 152, 52, 23, 4, 'BE', 1, 20020714160455);
-INSERT INTO score_packet VALUES (115, 828, 109, 141, 97, 22, 6, 'BE', 0, 20020714160455);
+INSERT INTO score_packet VALUES (115, 828, 109, 141, 97, 22, 6, 'BE', 1, 20020729224624);
 
