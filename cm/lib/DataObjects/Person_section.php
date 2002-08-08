707a699e67289deb23e88eb09bc267959dfa2d68
cyface <cyface>
2002-08-08 18:37:26 +0000

    Fixes in regard to working with score packets/.

--- cm/lib/DataObjects/Person_section.php
+++ cm/lib/DataObjects/Person_section.php
@@ -1,7 +1,7 @@
 <?
 /*
 * Table Definition for person_section
-* CVS Info: $Id: Person_section.php,v 1.5 2002/07/26 23:07:43 cyface Exp $
+* CVS Info: $Id: Person_section.php,v 1.6 2002/08/08 18:37:26 cyface Exp $
 */
 
 
@@ -19,15 +19,15 @@ class DataObjects_Person_section extends DB_DataObject {
     var $section_id;                      // int(11)  not_null unsigned
     var $complete_event_number;           // string(10)  not_null
     var $reg_type;                        // string(80)  not_null
-    var $score;                           // int(4)  
-    var $place;                           // int(4)  
-    var $judge;                           // string(10)  
-    var $price;                           // real(10) 
-	var $old_price;                       // real(10) 
+    var $score;                           // int(4)
+    var $place;                           // int(4)
+    var $judge;                           // string(10)
+    var $price;                           // real(10)
+	var $old_price;                       // real(10)
     var $score_packet_id;                 // int(11)  unsigned
-    var $packet_position;                 // int(4)  
-    var $judge_score;                     // int(4)  
-    var $scenario_score;                  // int(4)  
+    var $packet_position;                 // int(4)
+    var $judge_score;                     // int(4)
+    var $scenario_score;                  // int(4)
     var $convention_id;                   // int(11)  not_null unsigned
     var $last_modified;                   // timestamp(14)  not_null unsigned zerofill timestamp
 
@@ -38,53 +38,57 @@ class DataObjects_Person_section extends DB_DataObject {
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
-	
+
 	/*
 	* insert - Inserting a person section means that you are either registering them, or adding them to a packet.
 	*          As a result, there are a lot of things that have to happen when one is added.  That stuff happens here.
-	* 
+	*
 	*/
-	function insert() {		
+	function insert() {
 		if ($this->id) { //The id may have gotten set in includedSearch below
 			return DB_DataObject::update(); //Call the parent method
 		}
-		
+
 		return DB_DataObject::insert(); //Call the parent method
 	}
-	
+
 	function delete() {
-		//Find the section row that matches the complete event number that was input
-		$sectionObject = DB_DataObject::staticGet('DataObjects_Section',$this->section_id);
-		//echo '<PRE> Found Section Object'; print_r($sectionObject); echo '<PRE>';
-		
-		//Update the section's event fullness indicators
-		$sectionObject->num_registered--;
-		$sectionObject->update(); //Save the section record ASAP.
-		
-		//Now find the related person record, and update their total fee
-		$personObject = DB_DataObject::staticGet('DataObjects_Person',$this->person_id);
-		$personObject->total_fee -= $this->price;
-		$personObject->update();
-		
+		if ($this->reg_type != 'Score Packet') {
+			//Find the section row that matches the complete event number that was input
+			$sectionObject = DB_DataObject::staticGet('DataObjects_Section',$this->section_id);
+			//echo '<PRE> Found Section Object'; print_r($sectionObject); echo '<PRE>';
+
+			//Update the section's event fullness indicators
+			$sectionObject->num_registered--;
+			$sectionObject->update(); //Save the section record ASAP.
+
+			//Now find the related person record, and update their total fee
+			$personObject = DB_DataObject::staticGet('DataObjects_Person',$this->person_id);
+			$personObject->total_fee -= $this->price;
+			$personObject->update();
+		}
+
 		return DB_DataObject::delete(); //Call the parent method
 	}
-	
-	function update() {	
-		//Find the related person record, and update their total fee
-		$personObject = DB_DataObject::staticGet('DataObjects_Person',$this->person_id);
-		$personObject->total_fee -= $this->old_price;
-		$personObject->total_fee += $this->price;
-		$personObject->update();
-		
-		$this->old_price = $this->price; //old_price is used to - off the old price of an event when the price is updated
-		
+
+	function update() {
+		if ($this->reg_type != 'Score Packet') {
+			//Find the related person record, and update their total fee
+			$personObject = DB_DataObject::staticGet('DataObjects_Person',$this->person_id);
+			$personObject->total_fee -= $this->old_price;
+			$personObject->total_fee += $this->price;
+			$personObject->update();
+
+			$this->old_price = $this->price; //old_price is used to - off the old price of an event when the price is updated
+		}
+
 		return DB_DataObject::update(); //Call the parent method
 	}
-	
+
 	/*
 	* includedInsert - You are either registering them, or adding them to a packet.
 	*          As a result, there are a lot of things that have to happen.  That stuff happens here.
-	* 
+	*
 	* @param string inValue - search value
 	*/
 	function includedInsert($inValue) {
@@ -94,13 +98,13 @@ class DataObjects_Person_section extends DB_DataObject {
 		$sectionObject->complete_event_number = $inValue;
 		$sectionObject->convention_id = $this->convention_id;
 		$sectionObject->find();
-		
+
 		//Error if that complete event number wasn't found
 		if ($sectionObject->N == 0) {//Didn't find a match
 		require_once('PEAR.php');
 		return new PEAR_Error('That Complete Event Number Does Not Exist.');
 		}
-		
+
 		//If we got this far, we found a matching section
 		$sectionObject->fetch(); //Pull the matching record
 		//Check to make sure that the event is not full
@@ -108,25 +112,25 @@ class DataObjects_Person_section extends DB_DataObject {
 		require_once('PEAR.php');
 		return new PEAR_Error('Event Full');
 		}
-		
+
 		//Update the section's event fullness indicators
 		$sectionObject->num_registered++;
 		$sectionObject->update(); //Save the section record ASAP.
-		
+
 		$sectionObject->getLinks(); //Load the related event info
 		//echo '<PRE> Found Section Object'; print_r($sectionObject); echo '<PRE>';
-		
+
 		//Copy the default information from the section & event records to this object
 		$this->section_id = $sectionObject->id;
 		$this->event_id = $sectionObject->event_id;
 		$this->price = $sectionObject->_event_id->price;
 		$this->old_price = $sectionObject->_event_id->price; //old_price is used to minus off the old price of an event when the price is updated
-		
+
 		//Now find the related person record, and update their total fee
 		$personObject = DB_DataObject::staticGet('DataObjects_Person',$this->person_id);
 		$personObject->total_fee += $this->price;
 		$personObject->update();
-		
+
 		return DB_DataObject::insert(); //Call the parent method
 		return false; //If we got this far, something is wrong!
 	} //End function includedInsert
