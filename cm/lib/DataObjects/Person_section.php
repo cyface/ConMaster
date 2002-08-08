df33c0dbfd0b3c18bc55cff51f393592fc4e91f3
cyface <cyface>
2002-08-08 18:40:39 +0000

    Added latest fields.

--- cm/lib/DataObjects/Person_section.php
+++ cm/lib/DataObjects/Person_section.php
@@ -1,7 +1,7 @@
 <?
 /*
 * Table Definition for person_section
-* CVS Info: $Id: Person_section.php,v 1.6 2002/08/08 18:37:26 cyface Exp $
+* CVS Info: $Id: Person_section.php,v 1.7 2002/08/08 18:40:39 cyface Exp $
 */
 
 
@@ -17,13 +17,14 @@ class DataObjects_Person_section extends DB_DataObject {
     var $id;                              // int(11)  not_null primary_key unsigned auto_increment
     var $person_id;                       // int(11)  not_null unsigned
     var $section_id;                      // int(11)  not_null unsigned
-    var $complete_event_number;           // string(10)  not_null
+    var $event_id;                      // int(11)  not_null unsigned
     var $reg_type;                        // string(80)  not_null
     var $score;                           // int(4)
+    var $prorated_score;                           // int(4)
     var $place;                           // int(4)
     var $judge;                           // string(10)
     var $price;                           // real(10)
-	var $old_price;                       // real(10)
+    var $old_price;                       // real(10)
     var $score_packet_id;                 // int(11)  unsigned
     var $packet_position;                 // int(4)
     var $judge_score;                     // int(4)
