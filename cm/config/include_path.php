2b15d3e300f6c13ac5800773fd048066b0507408
cyface <cyface>
2002-10-17 21:39:48 +0000

    Added new include_path stuff.

---
+++ cm/config/include_path.php
@@ -0,0 +1,18 @@
+<?php
+
+	$path_components = array();
+
+	array_push ($path_components, './lib');
+	array_push ($path_components, './lib/pear');
+
+	//Combine the path components with the appropriate OS-specific separator
+	if ( stristr(ini_get("include_path"), ';') ) {
+		$path = ';' . join ($path_components,';');
+	} else {
+		$path = ':' . join ($path_components,':');
+	}
+
+	ini_set('include_path', ini_get("include_path") . $path);
+
+	//echo ini_get("include_path");  //Debugging only
+?>
